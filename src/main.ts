98a528c1ec7acca50809a999ef2e1a66e1ecb64f
sapunethmini <sapuninethmini888@gmail.com>
2025-08-17 22:38:04 +0530

    add vercle .json file

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
+import { environment } from './environments/environment.prod';
+
+const apiUrl = (window as any)['API_URL'] || environment.apiUrl;
+environment.apiUrl = apiUrl;
 
 bootstrapApplication(AppComponent, {
   providers: [
